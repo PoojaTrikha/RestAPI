fac8435a20f0e1b951bf845a1ef5d3168141110a
Pooja <pooja.kaul2015@gmail.com>
2018-09-14 23:45:29 +0200

    first commit

---
+++ game/src/index.ts
@@ -0,0 +1,17 @@
+import 'reflect-metadata'
+import {createKoaServer} from 'routing-controllers'
+import PageController from './pages/controller'
+import setupDb from './db'
+
+const app = createKoaServer({
+  cors: true,
+  controllers: [
+    PageController
+  ]
+})
+
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
